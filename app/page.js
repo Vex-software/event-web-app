ed3f1f9d508c1844e7944fd9573bba9b008ac5c9
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-03-28 18:33:17 +0300

    update

--- app/page.js
+++ app/page.js
@@ -12,34 +12,7 @@ export default function Home() {
 
   return (
     <main>
-      <div className='p-2'>
-        <div className='mb-4'>
-            <Button>Deneme</Button>
-        </div>
-
-        <div className='mb-4'>
-          <Card></Card>
-        </div>
-
-        <div className='mb-4'>
-          <CardSoon title='Eypio Konseri!' day='3'></CardSoon>
-        </div>
-
-        <div className='mb-4'>
-         <Input label="First Name" placeholder="First Name"></Input>
-        </div>
-
-        <div className='mb-4'>
-         <Checkbox id="checkbox" name="message" message="Lorem ipsum dolor sit amet."></Checkbox>
-        </div>
-
-        <div className='mb-4'>
-         <InputPass label="Password" placeholder="Password"></InputPass>
-        </div>
-
-        <div className='mb-4'>
-         <Toggle>Online</Toggle>
-        </div>
+      <div className='p-10'>
 
         <div className='mb-4'>
          <Dropdown></Dropdown>
