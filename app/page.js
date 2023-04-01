0a22d3465a0c7edd2c6092df44142ab2ea4d74b7
servet <servetarslan.a@gmail.com>
2023-04-01 18:44:25 +0300

    priveew card on index

--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-
-import { Button, Card, CardSoon, Input, Checkbox, InputPass, Toggle, Dropdown } from '@/components'
-
-
-
+import { Card } from "@/components";
 
 export default function Home() {
-
+  let data = {
+    "title": "irure cillum sit ad sit voluptate",
+    "image": "https://picsum.photos/400/300",
+    "date": "2022-09-18T11:36:49.552Z",
+    "description": "Lorem ea in ex quis officia sint do amet pariatur dolore laboris ",
+    "location": "Maslak Arena",
+    "id": "c419fe48-5641-47e2-a245-766773bc7a62"
+  }
   return (
     <main>
       <div className='p-10'>
 
         <div className='mb-4'>
-         <Dropdown></Dropdown>
+          <Card data={data} />
         </div>
       </div>
     </main>
