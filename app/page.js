71c90a5009825df947b7ff52e85dc0a7c1d3593c
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-03 16:49:10 +0300

    some updates

--- app/page.js
+++ app/page.js
@@ -1,23 +1,14 @@
-
 import React from 'react'
-import { Datepicker, Filter } from '@/components'
+import { Filter } from '@/components'
+
+
 
 export default function Home()
 {
-  let data = {
-    "title": "irure cillum sit ad sit voluptate",
-    "image": "https://picsum.photos/400/300",
-    "date": "2022-09-18T11:36:49.552Z",
-    "description": "Lorem ea in ex quis officia sint do amet pariatur dolore laboris ",
-    "location": "Maslak Arena",
-    "id": "c419fe48-5641-47e2-a245-766773bc7a62"
-  }
   return (
     <main>
-      <div className='p-10'>
-        <div className='mb-4'>
-          <Filter></Filter>
-        </div>
+      <div className='p-5'>
+
       </div>
     </main>
   )
