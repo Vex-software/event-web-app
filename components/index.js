8f6dc7f1257752a209438837fc3217f8a2452e7a
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-03 15:19:36 +0300

    new exports

--- components/index.js
+++ components/index.js
@@ -7,8 +7,9 @@ export { default as Input } from './form/input/default'
 export { default as Checkbox } from './form/checkbox/index.jsx'
 export { default as InputPass } from './form/input/password'
 export { default as Toggle } from './toggle'
-export { default as Dropdown } from './filter/index.jsx'
+export { default as Filter } from './filter/index.jsx'
 export { default as Title } from './title/title.jsx'
+export { default as Datepicker } from './date-range/index.jsx'
 
 
 
