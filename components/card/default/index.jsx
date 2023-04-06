2beab3168f225659eb6a98e2e5b70892dff0db32
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-07 01:49:09 +0300

    Update Card Component

--- components/card/default/index.jsx
+++ components/card/default/index.jsx
@@ -1,29 +1,34 @@
 import Image from 'next/image';
 import React from 'react'
+import Link from 'next/link';
 import { GoLocation } from 'react-icons/go';
 import { DateTime } from 'luxon'
 
 
-function Card({ data }) {
+function Card({ url = "/", image, title, date, description, location })
+{
 
-    const { image, title, date, description, location } = data
+
 
     return (
-        <div className='w-[18rem] active:shadow-[-5px_-5px_0px_1px_rgba(0,34,186,1)] hover:shadow-[6px_6px_0px_1px_rgba(0,34,186,1)] cursor-pointer font-["Raleway"] border-2  border-[rgba(0,34,186,0.3)] rounded-md'>
-            <div>
-                <Image className='w-full rounded-tl-md rounded-tr-md object-cover' src={image} width="285" height="184" alt={title}></Image>
+        <div className=' md:w-[18rem] md:h-[22rem] w-[14rem] h-[14rem] cursor-pointer font-["Raleway"] border-2  border-[#4f4cee83] rounded-md'>
+            <div className='w-full md:h-52 h-[8rem]'>
+                <Link href={url}><img className='w-full h-full rounded-tl-md rounded-tr-md layout object-center object-cover' src={image} alt={title} /></Link>
             </div>
-            <div className='w-full p-4 flex items-center'>
+            <div className='w-full  p-4 flex items-center'>
                 <div className='mr-5'>
-                    <p className='text-center text-[1.2rem] font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('d')}</p>
-                    <p className='text-center text-[1.2rem]  font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('MMM')}</p>
+                    <Link href={url}>   <p className='text-center text-[1.2rem] font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('d')}</p></Link>
+                    <Link href={url}>   <p className='text-center text-[1.2rem]  font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('MMM')}</p></Link>
                 </div>
                 <div className='leading-7'>
-                    <h4 className='font-bold '>{title}</h4>
-                    <p className='text-[14px]'>{description}</p>
-                    <div className='flex items-center'>
-                        <GoLocation className='mr-2 font-bold' />
-                        <p className='font-medium'>{location}</p>
+                    <div className='w-36'>
+
+                        <Link href={url}>   <h4 className='font-bold truncate'>{title}</h4></Link>
+                    </div>
+                    <Link href={url}> <p className='md:block hidden text-[14px]'>{description}</p></Link>
+                    <div className='flex items-center w-36'>
+                        <Link href={url}><GoLocation className='mr-2 font-bold' /></Link>
+                        <Link href={url}><p className='text-sm font-medium truncate'>{location}</p></Link>
                     </div>
                 </div>
             </div>
