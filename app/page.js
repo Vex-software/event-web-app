d2f4fa665c0a7f2f7331f6edfb6fea57da6cf1d1
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-06 03:34:37 +0300

    Some Updates

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Filter, Alert, Carousel } from '@/components'
+import { Filter, Alert, Carousel, Banner, Date, Community, Information } from '@/components'
 import { Photos } from '@/mocks'
 
 export default function Home()
 {
   return (
-    <main>
-      <div className='w-5/6 mx-auto '>
-        <Carousel items={Photos} />
+    <main className='w-4/5 mx-auto'>
+      <div className='w-full'>
+        <Banner />
+        <Community>Dance Community</Community>
+        <Date>Eylül 22, 2023 · 20.00</Date>
+      </div>
+      <div className='w-full'>
+        <Information
+          duration="20.00 - 21.56 1 hour 56 minutes"
+          audience="This movie is suitable for
+          audience aged 12  and above"
+          attention="Face mask and social distancing
+          are mandatory outside the car."
+          location="Any Street, Any Neighborhood Merkez / Kutahya (6:45 Pub)"
+        />
       </div>
     </main>
   )
--- components/index.js
+++ components/index.js
@@ -12,6 +12,10 @@ export { default as Title } from './title/title.jsx'
 export { default as Datepicker } from './date-range/index.jsx'
 export { default as Carousel } from './carousel/index.jsx'
 export { default as Alert } from './alerts/index.jsx'
+export { default as Banner } from './eventDetail/banner/index.js'
+export { default as Date } from './eventDetail/date/index.jsx'
+export { default as Community } from './eventDetail/community/index.jsx'
+export { default as Information } from './eventDetail/information/index.jsx'
 
 
 
