8ca8a0bc28666a606bd5589a91c17efc2ba455cf
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-09 21:43:39 +0300

    Update Login Page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,22 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button, Card, CardSoon, Input, Checkbox, InputPass, Toggle, Dropdown } from '@/components'
+import { Button, Title, Card, CardSoon, Input, Checkbox, InputPass, Toggle, Dropdown } from '@/components'
 
 
 
-export default function Home() {
+export default function Home()
+{
   return (
     <main >
-      <div className='font-[Raleway] w-8/12 mx-auto  lg:w-4/12  mt-12 mb-12'>
+      <div className='font-[Jost] w-8/12 mx-auto md:w-1/3  mt-12 mb-12'>
 
 
-        <div className='mb-8'>
-          <h4 className='font-bold text-[2rem] text-[#7F7DF3]'>Log In</h4>
+        <div className='mb-12'>
+          <Title color="secondary" size="large">Log In</Title>
         </div>
         <form>
-          <div>
+          <div className=''>
             <div className='mb-6'>
-              <Input type="email" label="Email" name="email" placeholder="Email"/>
+              <Input type="email" label="Email" name="email" placeholder="Email" />
             </div>
 
             <div className=''>
@@ -24,14 +25,16 @@ export default function Home() {
             </div>
           </div>
 
-          <div className='mb-6 text-right text-sm text-[#4f4cee]'>
-            <Link href="/">Forgot Password?</Link>
+          <div className='mt-3 my-6 flex justify-between  text-[#4f4cee]'>
+            <Checkbox name="rememberMe" message="Remember Me" />
           </div>
-        
-            <Button >Log In</Button>
+
+          <Button >Log In</Button>
         </form>
-        <div className='mt-6 text-center text-[#4f4cee] text-md'>
-            <Link href="/">Don&apos;t You Have an Account?</Link>
+        <div className='mt-3 text-center flex flex-col text-[#4f4cee] text-md'>
+          <Link href="/" className='text-sm md:text-base mb-3 text-[#7F7DF3]'>Forgot Password?</Link>
+          <hr />
+          <Link href="/signup" className='mt-3'>Don&apos;t You Have an Account?</Link>
         </div>
 
       </div>
