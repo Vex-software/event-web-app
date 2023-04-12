3b4bff9fb2a076601ab4d3ca49c251f2368b3ebd
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-13 02:05:13 +0300

    Update Event Page

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,12 +1,11 @@
 "use client"
-// import React from 'react'
 import React, { useRef, useEffect } from 'react'
 import { Button, Title, Card, CardSoon, Input, Checkbox, InputPass, Toggle, Dropdown, Filter } from '@/components'
+import { Events } from '@/mocks'
 
 function index()
 {
     const filterRef = useRef(null); // create a ref for the filter component
-
     useEffect(() =>
     {
         const handleScroll = () =>
@@ -14,15 +13,13 @@ function index()
             const scrollY = window.scrollY;
             if (scrollY >= 100)
             {
-                filterRef.current.classList.add('md:fixed', 'md:top-0', 'md:left-0');
+                filterRef.current.classList.add('md:fixed', 'md:top-0', 'md:left-0', 'lg:static',);
             } else
             {
-                filterRef.current.classList.remove('md:fixed', 'md:top-[4.5rem]', 'md:left-0');
+                filterRef.current.classList.remove('md:fixed', 'md:top-[4.5rem]', 'md:left-0', 'lg:static');
             }
         };
-
         window.addEventListener('scroll', handleScroll);
-
         return () =>
         {
             window.removeEventListener('scroll', handleScroll);
@@ -30,30 +27,28 @@ function index()
     }, []);
     return (
         <main>
-            <div className='w-full flex flex-col px-1 md:flex-row md:justify-end md:mt-16 mb-20'>
-                <div className='md:h-screen md:w-[30%]' ref={filterRef}>
+            <div className='lg:w-4/5 lg:mx-auto w-full flex flex-col px-1 md:flex-row md:justify-end md:mt-16 mb-20'>
+                <div className='md:h-screen md:w-[30%] lg:w-1/5' ref={filterRef}>
                     <div className='my-5 px-2 w-full'>
                         <Filter />
                     </div>
                 </div>
-                <div className='md:w-[70%]'>
-                    <div className='w-full flex flex-col  '>
-                        <div className=' w-full flex flex-row justify-between'>
-                            <div className='w-3/5 p-1'>
-                                {/* <Card url="/" image="/card/eventCard/card-1.svg" title="Örnek Başlık" date="2023-04-12T10:30:00.000Z" description="Bu bir örnek açıklamadır." location="İstanbul, Türkiye" /> */}
-                            </div>
-                            <div className='w-3/5 p-1'>
-                                <Card url="/" image="/card/eventCard/card-2.svg" title="Örnek Başlık" date="2023-04-12T10:30:00.000Z" description="Bu bir örnek açıklamadır." location="İstanbul, Türkiye" />
-                            </div>
-                        </div>
-                        <div className=' w-full flex flex-row justify-between'>
-                            <div className='w-3/5 p-1'>
-                                <Card url="/" image="/card/eventCard/card-3.svg" title="Örnek Başlık" date="2023-04-12T10:30:00.000Z" description="Bu bir örnek açıklamadır." location="İstanbul, Türkiye" />
-                            </div>
-                            <div className='w-3/5 p-1'>
-                                <Card url="/" image="/card/eventCard/card-4.svg" title="Örnek Başlık" date="2023-04-12T10:30:00.000Z" description="Bu bir örnek açıklamadır." location="İstanbul, Türkiye" />
-                            </div>
-                        </div>
+                <div className='md:w-[70%] lg:w-4/5'>
+                    <div className='w-full flex flex-row flex-wrap  '>
+                        {
+                            Events.map((i, index) =>
+                                <div key={index} className='w-1/2 lg:w-1/3 p-1'>
+                                    <Card
+                                        url={i.url}
+                                        image={i.image}
+                                        title={i.title}
+                                        date={i.date}
+                                        description={i.description}
+                                        location={i.location}
+                                    />
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
