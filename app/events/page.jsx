4a80e88b03461410f8fa10b3da1b5fb40ec1fd41
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-12 14:32:37 +0300

    Create Events Page

---
+++ app/events/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Button, Title, Card, CardSoon, Input, Checkbox, InputPass, Toggle, Dropdown, Filter } from '@/components'
+
+
+function index()
+{
+    return (
+        <main className='m-10'>
+            <Filter ></Filter>
+        </main>
+    )
+}
+
+export default index
