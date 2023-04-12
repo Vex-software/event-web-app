b5798d4b29b07ce83403362bf470c08aa6a3b2d1
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-04-12 21:45:44 +0300

    Update Card Component

--- components/card/default/index.jsx
+++ components/card/default/index.jsx
@@ -8,26 +8,23 @@ import { DateTime } from 'luxon'
 function Card({ url = "/", image, title, date, description, location })
 {
 
-
-
     return (
-        <div className=' md:w-[18rem] md:h-[22rem] w-[14rem] h-[14rem] cursor-pointer font-["Raleway"] border-2  border-[#4f4cee83] rounded-md'>
-            <div className='w-full md:h-52 h-[8rem]'>
+        <div className=' w-full h-full cursor-pointer font-["Raleway"] border-2  border-[#4f4cee83] rounded-md'>
+            <div className='w-full h-[60%]'>
                 <Link href={url}><img className='w-full h-full rounded-tl-md rounded-tr-md layout object-center object-cover' src={image} alt={title} /></Link>
             </div>
-            <div className='w-full  p-4 flex items-center'>
-                <div className='mr-5'>
-                    <Link href={url}>   <p className='text-center text-[1.2rem] font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('d')}</p></Link>
-                    <Link href={url}>   <p className='text-center text-[1.2rem]  font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('MMM')}</p></Link>
+            <div className='w-full flex items-center'>
+                <div className='w-[20%] '>
+                    <Link href={url}>   <p className='text-center font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('d')}</p></Link>
+                    <Link href={url}>   <p className='text-center font-bold'>{DateTime.fromISO(date).setLocale('tr').toFormat('MMM')}</p></Link>
                 </div>
-                <div className='leading-7'>
+                <div className='w-[80%] leading-7'>
                     <div className='w-36'>
-
-                        <Link href={url}>   <h4 className='font-bold truncate'>{title}</h4></Link>
+                        <Link href={url}><h4 className='font-bold text-sm truncate'>{title}</h4></Link>
                     </div>
-                    <Link href={url}> <p className='md:block hidden text-[14px]'>{description}</p></Link>
+                    <Link href={url}> <p className='md:block hidden'>{description}</p></Link>
                     <div className='flex items-center w-36'>
-                        <Link href={url}><GoLocation className='mr-2 font-bold' /></Link>
+                        <Link href={url}><GoLocation className='mr-1 text-sm font-bold' /></Link>
                         <Link href={url}><p className='text-sm font-medium truncate'>{location}</p></Link>
                     </div>
                 </div>
